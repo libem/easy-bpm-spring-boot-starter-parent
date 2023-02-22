bfd8e85470057c7809b9716ca9a219ebc53e8a1d
lb@szetst.com <lb@szetst.com>
2023-02-22 17:15:07 +0800

    * 本机环境配置

--- easy-bpm-auth/src/main/java/com/pig/easy/bpm/auth/AuthApplication.java
+++ easy-bpm-auth/src/main/java/com/pig/easy/bpm/auth/AuthApplication.java
@@ -2,12 +2,15 @@ package com.pig.easy.bpm.auth;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import java.util.concurrent.CountDownLatch;
 
 //@EnableEasyBpmAuth(scanBasePackages ={"com.pig.easy.bpm.auth.mapper"})
-@SpringBootApplication
+//@SpringBootApplication
+//排除配置类, 多数据源定义造成循环依赖
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AuthApplication {
 
 	public static void main(String[] args) {
